5ff96164a4a2d548e2a6070403914ed0caa82868
Submariness <49424747+Submariness@users.noreply.github.com>
2019-04-11 17:04:04 +0800

    Create activity.java

---
+++ activity.java
@@ -0,0 +1,9 @@
+public class MainActivity extends Activity{
+  
+  @Override
+  protected void onCreate(Bundle savedInstanceState){
+    // TODO: Implement this method
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+    
+}
