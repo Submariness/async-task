279e561d2e0044af1157e221d19705c0363d8511
Submariness <49424747+Submariness@users.noreply.github.com>
2019-04-11 17:12:52 +0800

    Update activity.java

--- activity.java
+++ activity.java
@@ -1,9 +1,19 @@
 public class MainActivity extends Activity{
   
+  private ImageView mImage;
+  private Button mButton;
+  
   @Override
   protected void onCreate(Bundle savedInstanceState){
     // TODO: Implement this method
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
     
+    initView():
+}
+
+// 初始化
+private void initView(){
+  mImage = (ImageView)findViewById(R.id.mImageView);
+  mButton = (Button)findViewById(R.id.mButton);
 }
