046d45bbc5c0d13c5865ceac83f528f184d2796d
Submariness <49424747+Submariness@users.noreply.github.com>
2019-04-15 17:15:44 +0800

    Update activity.java

--- activity.java
+++ activity.java
@@ -1,9 +1,18 @@
-public class MainActivity extends Activity {
-  
-  public static final String = "http://img1.gtimg.com/sports/pics/hv1/254/243/2090/135964469.jpg";
+package com.mycompany.myapp;
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.graphics.*;
+
   /**
    *  @author Submarine
    */
+
+public class MainActivity extends Activity {
+  
+  public static final String = "http://img1.gtimg.com/sports/pics/hv1/254/243/2090/135964469.jpg";
+  
   private ImageView mImage;
   private Button mButton;
   
@@ -14,20 +23,21 @@ public class MainActivity extends Activity {
     setContentView(R.layout.main);
     
     initView():
-}
+  }
 
-// 初始化
-private void initView(){
-  mImage = (ImageView)findViewById(R.id.mImageView);
-  mButton = (Button)findViewById(R.id.mButton);
+  // 初始化组件
+  private void initView(){
+    mImage = (ImageView)findViewById(R.id.mImageView);
+    mButton = (Button)findViewById(R.id.mButton);
   
-  mButton.setOnClickListener(new OnClickListener(){
+    mButton.setOnClickListener(new OnClickListener(){
     
-      @Override
-      public void onClick(View v){
-        new MyAsyncTask().execute(mUrl);
-      }
-  });
+        @Override
+        public void onClick(View v){
+          new MyAsyncTask().execute(mUrl);
+        }
+    });
+  }
   
   // new 一个内部的异步任务类
   class MyAsyncTask extends AsyncTask<String, Void, Bitmap>{
